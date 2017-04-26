fe103b4fac2a7a99e3abac34ef03d07496cbf3e2
Matt Mitchell <mmitche@microsoft.com>
2017-04-26 07:55:15 -0700

    Add comment regarding FilePath caching

--- src/main/java/hudson/plugins/ws_cleanup/Wipeout.java
+++ src/main/java/hudson/plugins/ws_cleanup/Wipeout.java
@@ -27,15 +27,12 @@ import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import hudson.FilePath;
 import hudson.model.Computer;
-import hudson.remoting.VirtualChannel;
 import jenkins.model.Jenkins;
 import org.jenkinsci.plugins.resourcedisposer.AsyncResourceDisposer;
 import org.jenkinsci.plugins.resourcedisposer.Disposable;
 
-import javax.annotation.CheckForNull;
 import javax.annotation.Nonnull;
 
 /**
@@ -96,9 +93,16 @@ import javax.annotation.Nonnull;
         @Nonnull public State dispose() throws Throwable {
             Jenkins j = Jenkins.getInstance();
             if (j == null) return State.TO_DISPOSE; // Going down?
-                
+
+            // We grab the computer and file path here each time.  Caching the file path is
+            // dangerous because a FilePath contains a Channel, implying
+            // the Channel is still around. In cases where you have a disconnect/reconnect,
+            // machine that was altogether discarded, etc. this method would simply throw
+            // and retry over and over again.  In certain cloud heavy installations,
+            // this might mean we leak Channel objects for discarded machines
+            // over time, eventually leading to an OOM.
             Computer computer = j.getComputer(node);
-            if (computer == null) return State.PURGED; // Removed or discarded cloud machine
+            if (computer == null) return State.PURGED;
             
             FilePath ws = new FilePath(computer.getChannel(), path);
             
